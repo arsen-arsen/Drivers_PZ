23472827de06eeaa5f66a36aed4dcc309f56358e
arsen-arsen <72999223+arsen-arsen@users.noreply.github.com>
2024-01-20 00:21:57 +0300

    Create modtr.c

---
+++ firstmodule/modtrans/modtr.c
@@ -0,0 +1,29 @@
+#include <linux/init.h>            
+#include <linux/module.h>        
+#include <linux/kernel.h> 
+#include <linux/printk.h>          
+ 
+MODULE_LICENSE("GPL");             
+MODULE_AUTHOR("Ivan");     
+MODULE_DESCRIPTION("A first Linux module");  
+MODULE_VERSION("0.1");              
+
+ //initializing module
+static int __init simple_init(void){
+   pr_info("Module had been included in the system\n");
+   
+   int Mult5(int d)
+{
+    int res;
+    res = d * 5;
+    return res;
+   return 0;
+}
+ 
+//cleanup module
+static void __exit simple_exit(void){
+   pr_info("Module had been disconnected.\n");
+}
+ 
+module_init(simple_init);
+module_exit(simple_exit);
